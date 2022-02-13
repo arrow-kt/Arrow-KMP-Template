89d183c978e8cd40cbc911a7c8dcef00b3d74454
i-walker <46971368+i-walker@users.noreply.github.com>
2022-02-13 20:18:29 +0100

    add yarn file and example for Laws

--- src/commonTest/kotlin/ExampleSpec.kt
+++ src/commonTest/kotlin/ExampleSpec.kt
@@ -8,12 +8,15 @@ import io.kotest.matchers.types.shouldBeTypeOf
 import io.kotest.property.Arb
 import io.kotest.property.arbitrary.int
 import io.kotest.property.arbitrary.list
+import io.kotest.property.arbitrary.map
+import io.kotest.property.arbitrary.positiveInt
 import io.kotest.property.arbitrary.string
 import io.kotest.property.arrow.core.MonoidLaws
 import io.kotest.property.arrow.core.either
 import io.kotest.property.arrow.core.functionAToB
 import io.kotest.property.arrow.laws.testLaws
 import io.kotest.property.arrow.optics.TraversalLaws
+import kotlin.jvm.JvmInline
 
 class ExampleSpec : StringSpec({
   "true shouldBe true" {
@@ -28,9 +31,10 @@ class ExampleSpec : StringSpec({
     // smart-cast abilities for arrow types
     Either.Right("HI").shouldBeRight().shouldBeTypeOf<String>()
 
-    // utilise builtin or costume Laws with Generators
+    // utilise builtin or costume Laws with Generators to verify behavior
     testLaws(
-      MonoidLaws.laws(Monoid.list(), Arb.list(Arb.string()))
+      MonoidLaws.laws(Monoid.list(), Arb.list(Arb.string())),
+      MonoidLaws.laws(Monoid.numbers(), Arb.numbers())
     )
 
     // optics Laws from arrow
@@ -44,3 +48,26 @@ class ExampleSpec : StringSpec({
     )
   }
 })
+
+fun Arb.Companion.numbers(): Arb<Numbers> =
+  Arb.positiveInt().map { it.toNumber() }
+
+fun Int.toNumber(): Numbers =
+  if (this <= 0) Zero else Suc(minus(1).toNumber())
+
+// natural numbers form a monoid
+fun Monoid.Companion.numbers(): Monoid<Numbers> =
+  object : Monoid<Numbers> {
+    override fun empty(): Numbers =
+      Zero
+
+    override fun Numbers.combine(b: Numbers): Numbers =
+      Suc(b)
+  }
+
+// natural numbers
+sealed interface Numbers
+object Zero : Numbers
+
+@JvmInline
+value class Suc(val value: Numbers) : Numbers
