c753e5d702cf46aa1cb568e2ab23bec6335d5545
Imran Malic Settuba <46971368+i-walker@users.noreply.github.com>
2022-04-23 21:46:07 +0200

    move to continuations

--- src/commonMain/kotlin/example.kt
+++ src/commonMain/kotlin/example.kt
@@ -3,7 +3,7 @@
 import arrow.core.Either
 import arrow.core.Either.Left
 import arrow.core.Either.Right
-import arrow.core.computations.either
+import arrow.core.continuations.either
 
 object Lettuce
 object Knife
