131030c9f8aaeb4aa51ce27b0ccecdd71a0fcee7
Alejandro Serrano Mena <trupill@gmail.com>
2025-04-22 21:19:54 +0200

    Updates April 2025

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
+
 plugins {
-  kotlin("multiplatform") version "1.8.20" apply true
-  id("io.kotest.multiplatform") version "5.5.4" apply true
-  id("com.google.devtools.ksp") version "1.8.20-1.0.11"
+  alias(libs.plugins.kotlin.multiplatform)
+  alias(libs.plugins.ksp)
 }
 
 group "org.example"
@@ -9,76 +10,55 @@ version "1.0"
 
 repositories {
   mavenCentral()
-  maven {
-    url = uri("https://oss.sonatype.org/content/repositories/snapshots")
-  }
-}
-
-// release candidate avoids a null pointer exception during optics generation
-val arrowVersion = "1.2.0-RC"
-dependencies {
-  add("kspCommonMainMetadata", "io.arrow-kt:arrow-optics-ksp-plugin:$arrowVersion")
 }
 
 kotlin {
   jvm()
 
-  js(IR) {
+  js {
     browser()
     nodejs()
   }
 
   linuxX64()
-
   mingwX64()
-
-  iosArm32()
   iosArm64()
   iosSimulatorArm64()
   iosX64()
   macosArm64()
   macosX64()
-  tvosArm64()
-  tvosSimulatorArm64()
-  tvosX64()
-  watchosArm32()
-  watchosArm64()
-  watchosSimulatorArm64()
-  watchosX64()
-  watchosX86()
+
+  applyDefaultHierarchyTemplate()
 
   sourceSets {
     commonMain {
       dependencies {
         kotlin.srcDir("build/generated/ksp/metadata/commonMain/kotlin")
-        implementation(kotlin("stdlib-common"))
-        implementation("io.arrow-kt:arrow-core:$arrowVersion")
-        implementation("io.arrow-kt:arrow-optics:$arrowVersion")
-        implementation("io.arrow-kt:arrow-fx-coroutines:$arrowVersion")
-        implementation("io.arrow-kt:arrow-optics:$arrowVersion")
-        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+        implementation(libs.coroutines.core)
+        implementation(libs.arrow.core)
+        implementation(libs.arrow.fxCoroutines)
+        implementation(libs.arrow.resilience)
+        implementation(libs.arrow.optics)
       }
     }
 
-    val kotestVersion = "5.5.4"
-    val kotestArrowVersion = "1.3.0"
     commonTest {
       dependencies {
-        implementation("io.kotest:kotest-property:$kotestVersion")
-        implementation("io.kotest:kotest-framework-engine:$kotestVersion")
-        implementation("io.kotest:kotest-assertions-core:$kotestVersion")
-        implementation("io.kotest.extensions:kotest-assertions-arrow:$kotestArrowVersion")
-        implementation("io.kotest.extensions:kotest-property-arrow:$kotestArrowVersion") // optional
-        implementation("io.kotest.extensions:kotest-property-arrow-optics:$kotestArrowVersion") // optional
-      }
-    }
-
-    val jvmTest by getting {
-      dependencies {
-        implementation("io.kotest:kotest-runner-junit5-jvm:5.3.0")
+        implementation(kotlin("test"))
+        implementation(libs.bundles.testing)
       }
     }
 
     val jvmMain by getting
+  }
+}
+
+dependencies {
+  add("kspCommonMainMetadata", libs.arrow.optics.ksp)
+}
+
+project.tasks.withType(KotlinCompilationTask::class.java).configureEach {
+  if(name != "kspCommonMainKotlinMetadata") {
+    dependsOn("kspCommonMainKotlinMetadata")
   }
 }
